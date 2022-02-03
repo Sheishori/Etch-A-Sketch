7afebb7544cdc787aff2273e98fc4919cb97c812
Sheishori <sheishori@gmail.com>
2022-02-03 15:41:39 +0100

    Generate a grid from square divs

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container");
+
+for (i = 0; i < 20; i++) {
+	const verticalLine = document.createElement("div");
+	verticalLine.classList.add("vertical-line");
+	container.appendChild(verticalLine);
+	for (j = 0; j < 20; j++) {
+		const square = document.createElement("div");
+		square.classList.add("square");
+		verticalLine.appendChild(square);
+	}
+}
